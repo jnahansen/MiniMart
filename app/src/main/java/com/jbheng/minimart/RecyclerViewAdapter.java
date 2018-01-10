747a9d39286fdf845cbe94eea865d2796b72fa76
John Hansen <jnahansen@gmail.com>
2018-01-10 07:26:55 -0800

    Refactoring. Added click listener for product list items.

--- app/src/main/java/com/jbheng/minimart/RecyclerViewAdapter.java
+++ app/src/main/java/com/jbheng/minimart/RecyclerViewAdapter.java
@@ -2,18 +2,20 @@ package com.jbheng.minimart;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Vector;
 
 /**
  * The {@link RecyclerViewAdapter} class.
  * <p>The adapter provides access to the items in the {@link ItemViewHolder}
- * or the {@link NativeExpressAdViewHolder}.</p>
+ * or the {@link }.</p>
  */
 class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     // A menu item view type.
@@ -22,13 +24,9 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     // An Activity's Context.
     private final Context mContext;
 
-    // The list of Native Express ads and menu items.
+    // The list of product items.
     private final Vector<Object> mRecyclerViewItems;
 
-    /**
-     * For this example app, the recyclerViewItems list contains only
-     * {@link Product} types.
-     */
     public RecyclerViewAdapter(Context context, Vector<Object> recyclerViewItems) {
         this.mContext = context;
         this.mRecyclerViewItems = recyclerViewItems;
@@ -36,33 +34,25 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     /**
      * The {@link ItemViewHolder} class.
-     * Provides a reference to each view in the menu item view.
+     * Provides a reference to each view in the product item view.
      */
     public class ItemViewHolder extends RecyclerView.ViewHolder {
-        private TextView menuItemName;
-        private TextView menuItemDescription;
-        private TextView menuItemPrice;
-        private TextView menuItemCategory;
-        private ImageView menuItemImage;
+        private TextView productItemName;
+        private TextView productItemDescription;
+        private TextView productItemPrice;
+        private TextView productItemCategory;
+        private ImageView productItemImage;
 
         ItemViewHolder(View view) {
             super(view);
-            menuItemImage = view.findViewById(R.id.menu_item_image);
-            menuItemName = view.findViewById(R.id.menu_item_name);
-            menuItemPrice = view.findViewById(R.id.menu_item_price);
-            menuItemCategory = view.findViewById(R.id.menu_item_category);
-            menuItemDescription = view.findViewById(R.id.menu_item_description);
-        }
-    }
+            productItemImage = view.findViewById(R.id.product_item_image);
+            productItemName = view.findViewById(R.id.product_item_name);
+            productItemPrice = view.findViewById(R.id.product_item_price);
+            productItemCategory = view.findViewById(R.id.product_item_category);
+            productItemDescription = view.findViewById(R.id.product_item_description);
 
-    /**
-     * The {@link NativeExpressAdViewHolder} class.
-     */
-    public class NativeExpressAdViewHolder extends RecyclerView.ViewHolder {
-
-        NativeExpressAdViewHolder(View view) {
-            super(view);
         }
+
     }
 
     @Override
@@ -90,27 +80,46 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     /**
-     * Replaces the content in the views that make up the menu item view and the
-     * Native Express ad view. This method is invoked by the layout manager.
+     * Replaces the content in the views that make up the product item view.
+     * This method is invoked by the layout manager.
      */
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 //        int viewType = getItemViewType(position);
 
-        ItemViewHolder menuItemHolder = (ItemViewHolder) holder;
-        Product menuItem = (Product) mRecyclerViewItems.get(position);
+        final ItemViewHolder productItemHolder = (ItemViewHolder) holder;
+        final Product productItem = (Product) mRecyclerViewItems.get(position);
 
-        // Get the menu item image resource ID.
-        String imageName = menuItem.getImageName();
+        // Get product item image resource ID.
+        String imageName = productItem.getImageName();
         int imageResID = mContext.getResources().getIdentifier(imageName, "drawable",
                 mContext.getPackageName());
 
         // Add the menu item details to the menu item view.
-        menuItemHolder.menuItemImage.setImageResource(imageResID);
-        menuItemHolder.menuItemName.setText(menuItem.getName());
-        menuItemHolder.menuItemPrice.setText(menuItem.getPrice());
-        menuItemHolder.menuItemCategory.setText(menuItem.getCategory());
-        menuItemHolder.menuItemDescription.setText(menuItem.getDescription());
+        productItemHolder.productItemImage.setImageResource(imageResID);       // todo: use Picasso here
+        productItemHolder.productItemName.setText(productItem.getName());
+        productItemHolder.productItemPrice.setText(productItem.getPrice());
+        productItemHolder.productItemCategory.setText(productItem.getCategory());
+        productItemHolder.productItemDescription.setText(productItem.getDescription());
+
+        // Set click listener on view
+        productItemHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Log.i("RVAdapter","Clicked on item");
+                    Toast.makeText(mContext, "Clicked on item " + productItem.getName(), Toast.LENGTH_SHORT).show();
+                    // todo: swap list for product fragment here
+//                    TextView tickerTv = (TextView) v.findViewById(R.id.stockId);
+//                    String ticker = tickerTv.getText().toString();
+//                    FragmentUtils.addFragment(mFragmentManager,
+//                            ContextMenuDialogFragment.newInstance(ticker,mBrokerName,Constants.DETAIL_DIALOG_TYPE_POSITION),
+//                            ContextMenuDialogFragment.TAG);
+                } catch (Exception e) {
+                    Log.e("RVAdapter","onClick: exception: ",e);
+                }
+            }
+        });
     }
 
 }
